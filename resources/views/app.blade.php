567b6bee145c99f176a388706af1f3162f641454
01luisfonseca <01luisfonseca@gmail.com>
2016-05-10 11:31:12 -0500

    Ajuste de archivos publicos

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
 <meta charset="utf-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 <title>Deportes</title>
-<script src='/assets/js/jquery-2.2.3.min.js'></script>
+<script src='/public/assets/js/jquery-2.2.3.min.js'></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.2.1/foundation.css">
 <script src='https://cdnjs.cloudflare.com/ajax/libs/foundation/6.2.1/foundation.min.js'></script>
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.min.js"></script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('app')
  
 @section('body')
-<script src='js/app.js'></script>
+<script src='/public/js/app.js'></script>
 <div ng-App='deportes'><app-dir></app-dir></div>
 @endsection
